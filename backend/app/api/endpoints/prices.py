3a77a202b57cb3d50fe4d5523aaaf065c68f6e52
rafinder1 <rafpro33@gmail.com>
2024-09-23 21:32:37 +0200

    change srtyle code - flake8

--- backend/app/api/endpoints/prices.py
+++ backend/app/api/endpoints/prices.py
@@ -3,15 +3,18 @@ import yfinance as yf
 
 router = APIRouter()
 
+
 @router.get("/")
 def get_stock_price(ticker: str, period: str = '1d'):
     """
     Fetches historical stock price data for a given ticker.
 
     Parameters:
-    - ticker (str): The stock ticker symbol (e.g., 'AAPL', 'GOOGL'). Ticker you can find on this page: https://finance.yahoo.com/
-    - period (str): The period for which to retrieve historical data (default is '1d'). Options include:
-        - '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'.
+    - ticker (str): The stock ticker symbol (e.g., 'AAPL', 'GOOGL').
+    You can find tickers on: https://finance.yahoo.com/
+    - period (str): The period for which to retrieve historical data
+    (default is '1d'). Options include: '1d', '5d', '1mo', '3mo',
+    '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'.
 
     Returns:
     - dict: Historical stock price data.
@@ -24,10 +27,15 @@ def get_stock_price(ticker: str, period: str = '1d'):
     try:
         stock = yf.Ticker(ticker)
         data = stock.history(period=period)
+
         if data.empty:
-            raise HTTPException(status_code=404, detail="No data for the given ticker or period.")
-        
+            raise HTTPException(
+                status_code=404,
+                detail="No data for the given ticker or period."
+            )
         return data.to_dict()
-    
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Data download error: {str(e)}")
+        raise HTTPException(
+            status_code=400,
+            detail=f"Data download error: {str(e)}"
+        )
